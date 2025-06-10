12b0d2e829e2f8208f7656500494843904adcdfd
Mradul999 <demorgans007@gmail.com>
2025-06-10 11:06:53 +0530

    added addtwonumbers code

---
+++ AddTwoNumbers.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class AddTwoNumbers{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+
+int a=sc.nextInt();
+int b=sc.nextInt();
+
+int add_function=add(a,b);
+
+System.out.println(add_function);
+
+
+}
+
+public static int add(int a,int b){
+
+
+
+return a+b;
+
+
+}
+
+
+}
