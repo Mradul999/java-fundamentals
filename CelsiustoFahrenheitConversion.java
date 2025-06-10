8e07bb9355726488037b779f9070b70b2bd7cc65
Mradul999 <demorgans007@gmail.com>
2025-06-10 11:21:57 +0530

    added CelsiustoFahrenheitConversion

---
+++ CelsiustoFahrenheitConversion.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class CelsiustoFahrenheitConversion{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+
+int temp_in_celcius=sc.nextInt();
+
+int temp_in_fahrenheit=(temp_in_celcius*9/5)+32;
+
+System.out.println(temp_in_fahrenheit);
+
+
+}
+
+
+
+
+}
