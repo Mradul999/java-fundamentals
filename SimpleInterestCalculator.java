6a842fb60a032a73c8b7fa033869bcdc23e06c33
Mradul999 <demorgans007@gmail.com>
2025-06-11 00:40:38 +0530

    commit

---
+++ SimpleInterestCalculator.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class SimpleInterestCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+  
+        double principal = sc.nextDouble();
+        
+        double rate = sc.nextDouble();
+        double time = sc.nextDouble();
+        double simpleInterest = (principal * rate * time) / 100;
+        System.out.println( simpleInterest);
+    }
+}
